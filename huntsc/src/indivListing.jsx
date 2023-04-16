7ad3f4f7b6b7b1d18ec703125b1ce2e90e6c7a98
Hallie Faust <hfaust@usc.edu>
2023-04-15 20:22:19 -0700

    new indiv listing

--- huntsc/src/indivListing.jsx
+++ huntsc/src/indivListing.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import './indivListing.css';
 import './App.css';
 
-function Events(props) {
-    console.log(props.start)
-    return (
-        <div className = "OneEvent">
-            <h1 className="font-montserrat">{props.title} </h1>
-            <h3>Time: {props.start}  - {props.end}</h3>
-            <h3>Location: {props.location} </h3>
-            <h3>Description: {props.description} </h3>
-            <h3>Oranizaion: {props.organization} </h3>
-        </div>
 
-    
+function Events(props) {
+    // console.log(props.start)
+    return (
+        <div className = "OneListing">
+            <div className = "header">
+                <h1>Venue Name: {props.name}</h1>
+                <h3>Fryft: {props.fryft} </h3>
+            </div>
+
+            <div className = "VenueInfo"> 
+                <h3 className="right">Contact: {props.contact}</h3>  
+                <h3 className="right">Other: {props.other} </h3>
+                <h3 className="right">Requirements: {props.requirements} </h3>
+                <h3 className="right">Security Deposit: {props.security-deposit} </h3>
+                <h3 className="right">Price: {props.venue_price} </h3>
+                <h3 className="right">Capacity: {props.capacity} </h3>
+            </div>
+
+            <li className = "button">
+                <Link to= "/Form-Requester">Request</Link>
+            </li>
+        </div>
     );
 }
-
-
-
 
 export default Events;
