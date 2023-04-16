bb9febcbcca9f6a986602eed9828a0731146dec4
Hallie Faust <hfaust@usc.edu>
2023-04-15 20:58:52 -0700

    edited indiv listing

--- huntsc/src/indivListing.jsx
+++ huntsc/src/indivListing.jsx
@@ -8,25 +8,22 @@ function Events(props) {
     return (
         <div className = "OneListing">
             <div className = "header">
-                <h1>Venue Name: {props.name}</h1>
-                <h3>Fryft: {props.fryft} </h3>
+                <h1>"{ props.name}"</h1>
+                <h3 className = "fryft">Fryft {props.fryft} </h3>
             </div>
 
             <div className = "VenueInfo"> 
+                <h3 className="right">Max Capacity: {props.capacity} </h3>
                 <h3 className="right">Contact: {props.contact}</h3>  
                 <h3 className="right">Other: {props.other} </h3>
                 <h3 className="right">Requirements: {props.requirements} </h3>
-<<<<<<< HEAD
-                <h3 className="right">Deposit: {props.deposit} </h3>
-=======
-                <h3 className="right">Security Deposit: {props.deposit} </h3>
->>>>>>> c158dbb3f0c06e5491c72cecf5f9b37f6a000046
-                <h3 className="right">Price: {props.venue_price} </h3>
-                <h3 className="right">Capacity: {props.capacity} </h3>
+                <h3 className="right">Security Deposit: ${props.deposit} </h3>
+                <h3 className="right">Price: ${props.venue_price} </h3>
+                
             </div>
 
             <li className = "button">
-                <Link to= "/Form-Requester">Request</Link>
+                <Link id="linked" to= "/Form-Requester">Request</Link>
             </li>
         </div>
     );
